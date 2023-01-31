25103a7d1060c816b5181e60d62ef2015bd47009
Megha <meghakaradbhajne21@gmail.com>
2023-01-31 16:45:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from "react";
+import "./style.css";
+import ReactPaginate from 'react-paginate';
 
-function App() {
+
+export default function App() {
+  const [users, setUsers] = React.useState([]);
+  const f = async () => {
+    const res = await fetch("https://reqres.in/api/users/");
+    const json = await res.json();
+    setUsers(json.data);
+  };
+  React.useEffect(() => {
+    f();
+  }, []);
+  const handlePageClick = () =>{
+    console.log("clicked");
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello ReqRes users!</h1>
+      <div className="flex">
+        {users.length &&
+          users.map((user) => {
+            return (
+              <div key={user.id}>
+                <p>
+                  <strong>{user.first_name}</strong>
+                </p>
+                <p>{user.email}</p>
+                <img key={user.avatar} src={user.avatar} />
+              </div>
+            );
+          })}
+           
+         <ReactPaginate
+        previousLabel={'previous'}
+         nextLabel={'next'}
+        //  breakLabel={'.. .'}
+         pageCount={25}
+          marginPagesDisplayed
+         onPageChange={handlePageClick}
+         containerClassName={'pagination  justify-content-center'}
+
+         pageClassName={'page-item'}
+         pageLinkClassName={'page-link'}
+        previousClassName={'page-item'}
+        previousLinkClassName={'page-link'}
+        nextClassName={'page-item'}
+        nextLinkClassName={'page-link'}
+        activeClassName={'active'}
+
+         />
+       </div> 
     </div>
   );
 }
-
-export default App;
